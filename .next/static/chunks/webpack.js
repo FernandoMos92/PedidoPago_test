02e0c708d798ab6bed9f9031da237f1a55b65038
mos <fernandomosp@gmail.com>
2022-06-05 17:46:29 -0300

    :sparkles: Styled main component in new position

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a7847d4013ea389"; }
+/******/ 		__webpack_require__.h = function() { return "5f280ab1277f360c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/new-position/index.tsx
+++ src/pages/new-position/index.tsx
@@ -1,15 +1,23 @@
 import styles from "./style.module.scss";
+import Link from 'next/link';
 
 export default function NewPosition() {
   return (
     <div className={styles.positionContainer}>
       <div className={styles.topContainer}>
-        <span>
-          <img src="/images/arrow-left.svg" alt="Arrow left" />
-        </span>
+        <Link href="/">
+          <a>
+            <span>
+              <img src="/images/arrow-left.svg" alt="Arrow left" />
+            </span>
+          </a>
+        </Link>
         <h1>Novo cargo</h1>
       </div>
 
+      <section className={styles.mainContainer}>
+        teste
+      </section>
     </div>
   );
 }
