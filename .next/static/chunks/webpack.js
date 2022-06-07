6d410e969ebd6b687e50f90290bf04a0e8fdc17b
mos <fernandomosp@gmail.com>
2022-06-07 00:05:23 -0300

    test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e4dc6458267d9c63"; }
+/******/ 		__webpack_require__.h = function() { return "b1d2f2dab4b13380"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
